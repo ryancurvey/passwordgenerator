a5efcc409d966cfc8ddc208435416e4ae612279a
Curvey <ryan.curvey@gmail.com>
2022-11-22 15:55:31 -0700

    adding password length prompts and confirm character types

--- Develop/script.js
+++ Develop/script.js
@@ -1,57 +1,52 @@
-// Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-alert("Create a New Password!");
+
+function generatePassword () {
 var characterLimit = prompt("How many characters in your new password?");
+
 if(characterLimit < 8) {
 alert ("please choose between 8-128 characters!");
+
 } else if (characterLimit > 128) {
   alert ("please choose between 8-128 characters!");
-// } else(characterLimit = "input") How to get this to the next level
 }
+  else console.log(characterLimit);
 
-var characterType = prompt("Would you like to include lower case letters?");
-if (characterType ===True) {
-includes = ("abcdefghijklmnopqrstuvwxyz")
-} else (characterType !== True);
- {
-  includes = ("ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()")
-}
+var characterType = confirm("Include uppercase letters?");
+var characterType = confirm("Include lowercase letters?");
+var characterType = confirm("Include numbers?");
+var characterType = confirm("Include special characters?");
 
-// nothing is happening aftert this
+  if (characterType === true)
+  console.log (yes)
 
-var characterType = prompt("Would you like to include upper case letters?");
-if (characterType ===True) {
-includes = ("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
-} else (characterType !== True); 
-{
-includes = ("abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()")
-}
+//   (characterLimit * characterType)
+//  random math to select characters--how do I select from each character group
 
-var characterType = prompt("Would you like to include numbers?");
-if (characterType ===True) {
-includes = ("0123456789")
-} else (characterType !== True);
- {
-includes = ("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()")
-}
-
-var characterType = prompt("Would you like to include special characters?");
-if (characterType ===True) {
-includes = ("!@#$%^&*()")
-} else (characterType !== True);
- {
-includes = ("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
-}
+//   if (confirm("Include lowercase letters?"))
 
 
-function createPassword () {
-  var characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()"
-  var characterLimit = ("")
-}
-for(let i = 0; i <= characterLimit.length; i++) {
-  characterLimit = Math.floor(Math.random() * characterLimit.length);
-  }
+//   var characterType = confirm("Include lowercase letters?");
+  
+//   if (characterType === true)
+//   console.log (yes)
+
+//   var characterType = confirm("Include numbers?");
+  
+//   if (characterType === true)
+//   console.log (yes)
+
+//   var characterType = confirm("Include special characters?");
+  
+//   if (characterType === true)
+//   console.log (yes)
+
+
+    
+    return "password generated and displayed"
+} 
+
+
 
 
 
@@ -69,10 +64,94 @@ generateBtn.addEventListener("click", writePassword);
 
 
 
+// create password
+// length 8-128 characters
+// Create function off of each (true/false) prompts for Upper, lowercase, numbers, special characters
+// All represented character types
+// display password that matched the criteria.
+
+
+
+
+// // Assignment Code
+// var generateBtn = document.querySelector("#generate");
+
+// alert("Create a New Password!");
+// var characterLimit = prompt("How many characters in your new password?");
+
+// if(characterLimit < 8) {
+// alert ("please choose between 8-128 characters!");
+
+// } else if (characterLimit > 128) {
+//   alert ("please choose between 8-128 characters!");
+
+// } else (null) 
+// // else(characterLimit = "input") How to get this to the next level
+
+
+// var characterType = confirm("Would you like to include lower case letters?");
+// if (characterType ===True) {
+// includes = ("abcdefghijklmnopqrstuvwxyz")
+// } else (characterType !== True);
+//  {
+//   includes = ("ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()")
+// }
+
+// // nothing is happening after this
+
+// var characterType = prompt("Would you like to include upper case letters?");
+// if (characterType ===True) {
+// includes = ("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+// } else (characterType !== True); 
+// {
+// includes = ("abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()")
+// }
+
+// var characterType = prompt("Would you like to include numbers?");
+// if (characterType ===True) {
+// includes = ("0123456789")
+// } else (characterType !== True);
+//  {
+// includes = ("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()")
+// }
+
+// var characterType = prompt("Would you like to include special characters?");
+// if (characterType ===True) {
+// includes = ("!@#$%^&*()")
+// } else (characterType !== True);
+//  {
+// includes = ("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
+// }
+
+
+// function generatePassword () {
+//   var characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()"
+//   var characterLimit = ("")
+// }
+// for(let i = 0; i <= characterLimit.length; i++) {
+//   var characterLimit = Math.floor(Math.random() * characterLimit.length);
+//   }
+
+
+
+// // Write password to the #password input
+// function writePassword() {
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
+
+//   passwordText.value = password;
+
+// }
+
+// // Add event listener to generate button
+// generateBtn.addEventListener("click", writePassword);
+
+
+
 
 
 // create password
 // length 8-128 characters
 // Create function off of each (true/false) prompts for Upper, lowercase, numbers, special characters
 // All represented character types
-// display password that matched the criteria
+// display password that matched the criteria.
