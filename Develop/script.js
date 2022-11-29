a717f4f9681cb8ba747ac41756b20d52e5f28964
Curvey <ryan.curvey@gmail.com>
2022-11-28 16:04:53 -0800

    removing redundant code

--- Develop/script.js
+++ Develop/script.js
@@ -23,7 +23,7 @@ function generatePassword() {
         else {
             console.log(characterLimit);
         }
-        console.log(condition);
+       
     } while (characterLimit < 8 || characterLimit > 128);
 
 
@@ -70,50 +70,6 @@ function generatePassword() {
 
 
 
-// if(Upper === true && !Lower && !Number && !Special) {
-//     possibleCharacters = characterUpper
-
-// } else if (Upper === true && Lower === true && !Number && !Special) {
-//     possibleCharacters = characterUpper + characterLower
-
-// } else if (Upper === true && !Lower && Number === true && !Special) {
-//     possibleCharacters = characterUpper + characterNumber
-
-// } else if (Upper === true && !Lower && !Number && Special === true) {
-//     possibleCharacters = characterUpper + characterSpecial
-
-// } else if (Upper === true && Lower === true && Number === true && !Special) {
-//     possibleCharacters = characterUpper + characterLower + characterNumber
-
-// } else if (Upper === true && Lower === true && !Number && Special === true) {
-//         possibleCharacters = characterUpper + characterLower + characterSpecial
-
-//  } else if (Upper === true && !Lower  && Number === true && Special === true) {
-//         possibleCharacters = characterUpper + characterNumber + characterSpecial
-
-// } else if (!Upper && Lower === true && !Number && !Special) {
-//     possibleCharacters = characterLower
-
-// } else if (!Upper && Lower === true && Number === true && !Special) {
-//     possibleCharacters = characterLower + characterNumber
-
-// } else if (!Upper&& Lower === true && !Number && Special === true) {
-//     possibleCharacters =  characterLower + characterSpecial
-
-// } else if (!Upper && Lower === true && Number === true && Special === true) {
-//     possibleCharacters = characterLower + characterNumber + characterSpecial
-
-// } else if (!Upper && !Lower && Number === true && !Special) {
-//     possibleCharacters = characterNumber
-
-// } else if (!Upper && !Lower && Number === true && Special === true) {
-//     possibleCharacters = characterNumber + characterSpecial
-
-// } else if (!Upper && !Lower && !Number && Special === true) {
-//     possibleCharacters = characterSpecial
-
-// } else  (Upper === true && Lower === true && Number === true && Special === true) 
-//     possibleCharacters = characterUpper + characterLower + characterNumber + characterSpecial  
 
 
 // Write password to the #password input
@@ -128,39 +84,3 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-// create password
-// length 8-128 characters
-// Create function off of each (true/false) prompts for Upper, lowercase, numbers, special characters
-// All represented character types
-// display password that matched the criteria.
-
-// if(Upper === true) {
-//     for(let i = 0; i <= characterLimit; i++) {
-//         var character = characterUpper.charAt(Math.floor(Math.random() * characterUpper.length));
-//       console.log(character)  
-//       } 
-// }
-
-// if(Lower === true) {
-//     for(let i = 0; i <= characterLimit; i++) {
-//         var character = characterLower.charAt(Math.floor(Math.random() * characterLower.length));
-//       console.log(character)  
-//       }
-// }
-
-// if(Number === true) {
-//     for(let i = 0; i <= characterLimit; i++) {
-//         var character = characterNumber.charAt(Math.floor(Math.random() * characterNumber.length));
-//       console.log(character)  
-//       }
-// }
-
-// if(Special === true) {
-//     for(let i = 0; i <= characterLimit; i++) {
-//         var character = characterSpecial.charAt(Math.floor(Math.random() * characterSpecial.length));
-//       console.log(character)  
-//       }
-// }
-
-//   if (characterType === true)
-//   console.log (yes)
